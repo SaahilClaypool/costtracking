2f67affb85293320e65302724289f9f88c56132e
SaahilClaypool <saahil@claypools.org>
2020-02-08 12:56:57 -0500

    Add main.js

---
+++ static/dist/2.js
@@ -0,0 +1,17 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],{
+
+/***/ 26:
+/***/ (function(module, exports) {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ 28:
+/***/ (function(module, exports) {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
